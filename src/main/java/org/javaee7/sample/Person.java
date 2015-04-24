109b0ddbf29b96bf4db17cb7e68c1f8b13049a93
Thomas Legoahec <thomas.legoahec@gmail.com>
2015-04-24 09:54:57 +0200

    on commit 3 hotfix

--- src/main/java/org/javaee7/sample/Person.java
+++ src/main/java/org/javaee7/sample/Person.java
@@ -3,7 +3,7 @@ package org.javaee7.sample;
 import javax.xml.bind.annotation.XmlRootElement;
 //On ajoute des trucs pour tester
 //on test le branchage
-// on fait le fix sur hotfix 2
+// on fait le fix sur hotfix 23
 /**
  * @author arungupta
  */
